15ed425788bb8ab3f8fe7a20c05918b9a282be63
Jose Gonzales <pythonhack22@gmail.com>
2023-11-17 11:39:48 -0500

    updateModelsBoolean

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 //ejecutar Dotenv
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successfull !")).catch((err) => { console.log(err) })
 
-app.use("/api/user", userRoute)
+
+
+app.use(express.json())
+app.use("/api/auth", authRoute)
 
 
 app.listen(process.env.PORT || 5000, () => {
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,11 @@ router.get("/usertest", (req, res) => {
     res.send("user test is Ok")
 })
 
+router.post("/userposttest", (req, res) => {
+    const username = req.body.username
+    res.send("your username is: " + username)
+})
+
 module.exports = router
+
+//https://www.youtube.com/watch?v=rMiRZ1iRC0A&t=418s
